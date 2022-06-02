7ad4c0f3591d28ad91d5595e63ec11c7d93b71fc
EmilAvramov <e_avramov@zoho.eu>
2022-06-03 00:28:44 +0300

    creating initial views

--- tutorial/blog/models.py
+++ tutorial/blog/models.py
@@ -1,7 +1,7 @@
-from msilib.schema import PublishComponent
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class PublishedManager(models.Manager):
@@ -35,3 +35,14 @@ class Post(models.Model):
 
     def __str__(self) -> str:
         return self.title
+
+    def get_absolute_url(self):
+        return reverse(
+            "blog:post_detail",
+            args=[
+                self.publish.year,
+                self.publish.month,
+                self.publish.day,
+                self.slug,
+            ],
+        )
