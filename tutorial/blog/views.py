fccb7e73dda633a5a4dcdb3da2e389920e7e3ac3
EmilAvramov <e_avramov@zoho.eu>
2022-06-05 01:59:03 +0300

    fixed static links and some view errors

--- tutorial/blog/views.py
+++ tutorial/blog/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.views.generic import ListView
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = "posts"
+    paginate_by = 3
+    template_name = "blog/post/list.html"
 
 
 def post_list(request):
@@ -16,4 +24,4 @@ def post_detail(request, year, month, day, post):
         publish__month=month,
         publish__day=day,
     )
-    return render(request, "blog/post/list.html", {"post": post})
+    return render(request, "blog/post/detail.html", {"post": post})
