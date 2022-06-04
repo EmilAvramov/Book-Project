7c583fb07eaa6e87a139c46a94e2d4c00159025c
EmilAvramov <e_avramov@zoho.eu>
2022-06-05 02:42:25 +0300

    added pages and fixes some errors

--- tutorial/blog/views.py
+++ tutorial/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.views.generic import ListView
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 class PostListView(ListView):
@@ -11,8 +12,19 @@ class PostListView(ListView):
 
 
 def post_list(request):
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3)
+    page = int(request.GET.get("page", "1"))
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
     posts = Post.published.all()
-    return render(request, "blog/post/list.html", {"posts": posts})
+    return render(
+        request, "blog/post/list.html", {"page": page, "posts": posts}
+    )
 
 
 def post_detail(request, year, month, day, post):
