8bec990a26f4af643505922424a0e19c00fbc8ef
EmilAvramov <e_avramov@zoho.eu>
2022-06-07 00:52:08 +0300

    form bug fixes

--- tutorial/blog/forms.py
+++ tutorial/blog/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 
 class EmailPostForm(forms.Form):
-    name = forms.CharField(max_length=25, help_text="25 characters max.")
-    email = forms.EmailField(help_text="Example: johndoe@email.com")
-    to = forms.EmailField(help_text="Example: johndoe@email.com")
+    name = forms.CharField(max_length=25)
+    email = forms.EmailField()
+    to = forms.EmailField()
     comments = forms.CharField(required=False, widget=forms.Textarea)
--- tutorial/blog/views.py
+++ tutorial/blog/views.py
@@ -22,9 +22,9 @@ def post_share(request, post_id):
         form = EmailPostForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            post_url = request.build_absolute_url(post.get_absolute_url())
+            post_url = request.build_absolute_uri(post.get_absolute_url())
             subject = f"{cd['name']} recommends you read {post.title}"
-            message = f"Read {post.title} at {post.url}\n\n{cd['name']} comments: {cd['comments']}"
+            message = f"Read {post.title} at {post_url}\n\n{cd['name']} comments: {cd['comments']}"
             send_mail(subject, message, "admin@myblog.com", [cd["to"]])
             sent = True
     else:
