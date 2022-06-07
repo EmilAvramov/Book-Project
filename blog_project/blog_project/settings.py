37247c7b7169e0b6711bcc7a4cc49472b8e26bce
EmilAvramov <e_avramov@zoho.eu>
2022-06-07 23:17:20 +0300

    fixed project name

--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "tutorial.urls"
+ROOT_URLCONF = "blog_project.urls"
 
 TEMPLATES = [
     {
@@ -71,7 +71,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "tutorial.wsgi.application"
+WSGI_APPLICATION = "blog_project.wsgi.application"
 
 
 # Database
