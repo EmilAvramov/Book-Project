58ab2fd159b986c619ff2dfcc8dd98b9286411a0
EmilAvramov <e_avramov@zoho.eu>
2022-06-08 00:00:37 +0300

    comment functionality p1

--- blog_project/blog/models.py
+++ blog_project/blog/models.py
@@ -6,8 +6,11 @@ from django.urls import reverse
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
-        return super(PublishedManager, self).get_queryset().filter(status="published")
-        
+        return (
+            super(PublishedManager, self)
+            .get_queryset()
+            .filter(status="published")
+        )
 
 
 class Post(models.Model):
@@ -43,3 +46,23 @@ class Post(models.Model):
                 self.slug,
             ],
         )
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments"
+    )
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+    
+    
+
+    class Meta:
+        ordering = ("created",)
+
+    def __str__(self) -> str:
+        return f"Comments by {self.name} on {self.post}"
--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,9 +1,7 @@
-import email
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Comment
 from django.views.generic import ListView
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 
 
@@ -60,4 +58,30 @@ def post_detail(request, year, month, day, post):
         publish__month=month,
         publish__day=day,
     )
-    return render(request, "blog/post/detail.html", {"post": post})
+
+    # list of active comments for this post
+    comments = post.comments.filter(active=True)
+    new_comment = None
+
+    if request.method == "POST":
+        # A comment was posted
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # Create Comment object but dont save to DB yet
+            new_comment = comment_form.save(commit=False)
+            # Assign post to comment
+            new_comment.post = post
+            # Save comment to DB
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+    return render(
+        request,
+        "blog/post/detail.html",
+        {
+            "post": post,
+            "comments": comments,
+            "new_comment": new_comment,
+            "comment_form": comment_form,
+        },
+    )
